26dc9f47c164dc943d802364a209e57f82bf09c6
Susana <sugupal@gmail.com>
2018-07-11 17:00:24 -0400

    changed dates, added SC member, fixed logo and not valid page

--- _site/contactus.php
+++ _site/contactus.php
@@ -104,7 +104,7 @@ mail($my_email,$subject,$message,$headers);
 </div>
         <div class="bg">
           <h1>3<sup>rd</sup> International Conference on Energy<br>Harvesting, Storage, and Transfer (EHST'19)</h1>
-          <p class="subhead">June 17 - 19, 2019 | Ottawa, Canada</p>
+          <p class="subhead">June 18 - 19, 2019 | Ottawa, Canada</p>
 
           <a href="../papers" class="bg-link">Submissions</a> <p class="dot">&middot;</p> <a href="../dates" class="bg-link">Important Dates</a> <p class="dot">&middot;</p> <a href="../registration" class="bg-link">Registration</a>
 
@@ -131,7 +131,7 @@ mail($my_email,$subject,$message,$headers);
 
         <div class="bg">
           <h1>3<sup>rd</sup> International Conference on Energy<br>Harvesting, Storage, and Transfer (EHST'19)</h1>
-          <p class="subhead">June 17 - 19, 2019 | Ottawa, Canada</p>
+          <p class="subhead">June 18 - 19, 2019 | Ottawa, Canada</p>
 
           <a href="../papers" class="bg-link">Submissions</a> <p class="dot">&middot;</p> <a href="../dates" class="bg-link">Important Dates</a> <p class="dot">&middot;</p> <a href="../registration" class="bg-link">Registration</a>
 
